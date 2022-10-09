78b126f6f63a083e268026b0004b22e2e80aa7d3
Mubaraq Yaqoub-Okponobi <oshiole94@gmail.com>
2022-10-09 05:18:55 +0100

    Complete Implementation of the Caesar Cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -6,16 +6,29 @@
 def caesar_cipher(str, shift_factor)
   result = []
   str.each_char do |value|
-    code = value.ord + shift_factor
-    if code > 122
-      code = "a".ord + (code - 123)
+    if value == "!" || (value == " ")
+      result.push(value)
+    else
+      if value.match?(/[A-Z]/)
+        value = (value.ord + 32).chr
+        code = value.ord + shift_factor
+        if code > 122
+          code = "a".ord + (code - 123)
+        end
+        result.push(code.chr.upcase)
+      else
+        code = value.ord + shift_factor
+        if code > 122
+          code = "a".ord + (code - 123)
+        end
+        result.push(code.chr)
+      end
     end
-    result.push(code.chr)
   end
   result.join
 end
 
-puts caesar_cipher("computer", 7)
+puts caesar_cipher("What a string!", 5)
 
 # You will need to remember how to convert a string into a number. ✅
 # Don’t forget to wrap from z to a. ✅
